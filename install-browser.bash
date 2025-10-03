7f696163542bef308eb7b87c1d13c74e828db9d0
Kenneth Lewenhagen <lewenhagen@live.com>
2025-10-03 13:19:01 +0200

    added git clone

--- install-browser.bash
+++ install-browser.bash
@@ -21,20 +21,21 @@ sudo apt install -y --no-install-recommends \
   openbox \
   chromium \
   unclutter-xfixes \
-  console-data 
+  console-data \
+  git
 
-echo "✅ 1. === Installation of prereq. done. ==="
+echo "[OK] 1. === Installation of prereq. done. (xorg, xserver-xorg-legacy, openbox, chromium, unclutter-xfixes, console-data, git) ==="
 
 
 # === 2. Allow X to start without root ===
 sudo sed -i 's/^allowed_users.*/allowed_users=anybody/' /etc/X11/Xwrapper.config || echo "allowed_users=anybody" >> /etc/X11/Xwrapper.config
 
-echo "✅ 2. === X is allowed to start without root ==="
+echo "[OK] 2. === X is allowed to start without root ==="
 
 
 # === 3. Add user to groups ===
 sudo usermod -aG video,audio,input $VSUSER
-echo "✅ 3. === $VSUSER is added to correct groups ==="
+echo "[OK] 3. === $VSUSER is added to correct groups ==="
 
 
 # === 4. Enable autologin for $VSUSER on TTY1 ===
@@ -44,7 +45,7 @@ cat << EOF | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /
 ExecStart=
 ExecStart=-/sbin/agetty --autologin $VSUSER --noclear %I $TERM
 EOF
-echo "✅ 4. === Autologin for tty1 is enabled for kiosk ==="
+echo "[OK] 4. === Autologin for tty1 is enabled for kiosk ==="
 
 
 # === 5. Setup .xinitrc ===
@@ -75,16 +76,16 @@ while true; do
     --start-fullscreen \
     --kiosk "http://localhost:3000"
 
-  echo "⚠️ Chromium crashed or exited, restarting in 5s..." >&2
+  echo "[WARNING] Chromium crashed or exited, restarting in 5s..." >&2
   sleep 5
 done
 EOF
 
-echo "✅ 5.1 === .xinitrc is set up for user $VSUSER ==="
+echo "[OK] 5.1 === .xinitrc is set up for user $VSUSER ==="
 
 
 chmod +x ~/.xinitrc
-echo "✅ 5.2 === .xinitrc has the right permission now. ==="
+echo "[OK] 5.2 === .xinitrc has the right permission now. ==="
 
 # === 6. Autostart X for $VSUSER ===
 cat > ~/.bash_profile <<'EOF'
@@ -93,5 +94,5 @@ if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
 fi
 EOF
 
-echo "✅ 6. === X is now on autostart for $VSUSER ==="
-echo "✅ Maxlew Videosystem browser setup complete."
+echo "[OK] 6. === X is now on autostart for $VSUSER ==="
+echo "[DONE] Maxlew Videosystem browser setup complete."
--- install-maxlew-vs.bash
+++ install-maxlew-vs.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/lewenhagen/maxlew_videosystem_node.git "$HOME/maxlew_videosystem"
+
+cd "$HOME/maxlew_videosystem"
+
+npm install
+
